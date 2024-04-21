a696056bbde060e4821654a58a5bba076ec36855
Prajwal Chikkagalagali <105001147+Prajwal07523@users.noreply.github.com>
2024-04-22 00:33:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 from cnnClassifier import logger
 
 
-# STAGE_NAME="Data Ingestion Stage"
+STAGE_NAME="Data Ingestion Stage"
 
-# try:
-#     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#     obj = DataIngestionTrainingPipeline()
-#     obj.main()
-#     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 STAGE_NAME = "Prepare base model"
 try: 
