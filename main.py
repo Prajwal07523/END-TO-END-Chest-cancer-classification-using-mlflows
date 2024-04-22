bc96ea4b6946d2cb5dca600940757c976f395c57
Prajwal07523 <01fe21bcs162@kletech.ac.in>
2024-04-22 14:29:33 +0530

    dvc updated

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from src.cnnClassifier import logger
-from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-from src.cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 from cnnClassifier import logger
 
@@ -16,6 +16,7 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
 
 STAGE_NAME = "Prepare base model"
 try: 
